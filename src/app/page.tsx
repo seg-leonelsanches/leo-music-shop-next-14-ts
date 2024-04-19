675b529fb8245bc2549091bf929614fc191c5e39
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2024-04-19 15:01:06 -0700

    - Making Segment work; - `.env` example.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+"use client"
+
+import * as React from "react";
+
 import Image from "next/image";
 
+import { useAnalytics } from "@/hooks";
+
 export default function Home() {
+  const analytics = useAnalytics();
+
+  React.useEffect(() => {
+    analytics.track("Index Page Viewed");
+  }, [analytics]);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
